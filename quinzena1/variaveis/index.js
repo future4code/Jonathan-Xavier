7cf2392b32d1a8bef3c78d242b57a157cb008f07
Jonathan <me.info.jonathan@gmail.com>
2021-05-20 21:00:38 -0300

    Aula03-Variaves-Exercicios

---
+++ quinzena1/variaveis/index.js
@@ -0,0 +1,78 @@
+//Pergunta 1
+//10
+//10 5
+
+//Pergunta 2
+//10 10 10
+
+//Pergunta 3
+// let horasTrabalhadasDia = prompt("Quantas horas você trabalha por dia?")
+// let valorRecebidoDia = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${valorRecebidoDia/horasTrabalhadasDia} por hora`)
+
+
+
+//Exercicio de escrita de codigo
+
+//Exercicio 1
+
+//Variaveis
+// const nome 
+// const idade 
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+//Undefined foi o tipo impresso, isso acontece pelo fato de nao ter valor atribuido para as variaveis
+
+// const nome = prompt("Qual o seu nome?")
+// const idade = Number(prompt("Qual a sua idade?"))
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+// //Variavel do tipo String no nome, por ser escrita e do tipo Number na idade, por ser um valor numerico
+
+// console.log("Ola", nome, ", voce tem", idade, "anos.")
+
+
+
+
+
+//Exercicio 2
+
+// const corCamiseta = ("Voce tem camiseta amarela?")
+// const dinheiroNoBolso = ("Voce tem dinheiro no bolso?")
+// const futebol = ("Voce gosta de futebol?")
+
+// const respostaCamiseta = "SIM" 
+// const respostaDinheiro = "SIM"
+// const respostaFutebol = "SIM"
+
+// console.log(corCamiseta, respostaCamiseta, dinheiroNoBolso, respostaDinheiro, futebol, respostaFutebol)
+
+
+
+//Exercicio 3
+
+// var a = 10
+// var b = 25
+// var aux = a
+
+// a = b
+// b = aux
+
+// console.log("O novo valor de a é", a) // O novo valor de a é 25
+// console.log("O novo valor de b é", b) // O novo valor de b é 10
+
+
+//Desafio
+
+var num1 = parseInt(prompt("Digite o primeiro numero: "))
+var num2 = parseInt(prompt("Digite o segundo numero: "))
+
+let resultadoSoma = num1 + num2
+let resultadoMultiplicacao = num1 * num2
+
+console.log("O primeiro numero somado ao segundo numero, resulta em: ", resultadoSoma)
+console.log("O primeiro numero multiplicado pelo segundo numero, resulta em: ", resultadoMultiplicacao)
