102d97748d8a562688c8225df111f88c3a214c64
Jonathan <me.info.jonathan@gmail.com>
2021-05-25 21:04:11 -0300

    Aula04 - Operadores - Exercicios

---
+++ quinzena1/operadores/index.js
@@ -0,0 +1,76 @@
+// Exercicios de interpretacao de codigo
+
+// EXERCICIO 1
+
+//a = false
+//b = false
+//c = true
+//d = boolean
+
+//---------------------------------------------------------------------------------------
+// EXERCICIO 2
+
+// Sera impresso a contatenaçao dois numeros ao inves de ser a soma efetivamente
+
+//---------------------------------------------------------------------------------------
+//EXERCICIO 3
+
+//Deve se colocar o Number, pois o programa entende como sendo uma string e contatena
+
+//----------------------------------------------------------------------------------------
+
+
+
+// Exercicios de escrita de codigo
+
+//1
+
+// let idadeUsuario = Number(prompt("Qual sua idade?"))
+// let idadeMelhorAmigo = Number(prompt("Qual a idade do seu melhor amigo (a)?"))
+// console.log("sua idade e maior que do seu melhor amigo?", idadeUsuario > idadeMelhorAmigo)
+// console.log(idadeUsuario - idadeMelhorAmigo)
+
+
+
+//2
+
+// let numPar = Number(prompt("Digite um numero par:"))
+// let resto = numPar = numPar % 2
+// console.log(resto)
+// Todo numero par o resultado sera 0
+// se o usuario enviar numero impar, ira sobrar sempre o numero 1
+
+
+
+//3
+
+
+// let idadeUsuario = Number(prompt("Qual sua idade?"))
+// let idadeMeses = idadeUsuario * 12
+// let idadeDias = idadeMeses * 365
+// let idadeHoras = idadeDias * 24
+// console.log("Sua idade em meses:",idadeMeses)
+// console.log("Sua idade em dias:",idadeDias)
+// console.log("Sua idade em horas:",idadeHoras)
+
+
+
+//4
+
+
+// let num1 = (prompt("Digite o primeiro numero"))
+// let num2 = (prompt("Digite o segundo numero"))
+
+// const maiorQue = num1 > num2
+// const igual = num1 === num2
+// const divisao1 = num1 % num2
+// const divisao2 = num2 % num1
+
+// console.log("O primeiro numero e maior que o segundo",maiorQue)
+// console.log("O primeiro numero e igual ao segundo",igual)
+// console.log("O primeiro numero e divisivel pelo segundo",divisao1)
+// console.log("O segundo numero e divisivel pelo primeiro",divisao2)
+
+
+
+
