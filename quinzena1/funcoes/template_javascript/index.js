0a551d80431e28ee36efd9fda13a7cfbf29808fd
Jonathan <me.info.jonathan@gmail.com>
2021-05-31 23:43:38 -0300

    inicio-exercicios-31-05

---
+++ quinzena1/funcoes/template_javascript/index.js
@@ -0,0 +1,59 @@
+// Exercícios de interpretação de código
+
+//1  
+
+//a) 10, 50
+
+//b) Não aparece nenhuma infirmação no console
+
+
+//---------------------------------------------
+
+//2
+
+//a) A função pega o texto do usuário, transforma tudo em letras minúsculas e procura no mesmo texto a palavra cenoura. Serve para ignorar erros de digitação do usuário entre caixa alta e baixa, além de mostrar um valor booleano no console true ou false caso encontre a palvra cenoura.
+
+//b) i. true
+//   ii. true
+//   iii. true 
+
+
+//---------------------------------------------------
+//---------------------------------------------------
+
+//Exercícios de escrita de código
+
+//1
+//a)
+
+// function informacoesPessoais(){
+//     console.log("Eu sou Jonathan, tenho 31 anos, moro em Palhoça, sou técnico de redes e estudante.")
+// }
+
+// informacoesPessoais()
+
+//b)
+
+function dadosPessoais(nome, idade, cidade, profissão){
+   console.log("Eu sou " + nome + " tenho " + idade + " anos, moro em " + cidade + " e sou " + profissão)
+}
+
+const informacoes = dadosPessoais(Jonathan, 31, Palhoca, tecnicoderedes)
+dadosPessoais()
+
+
+
+//-----------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
