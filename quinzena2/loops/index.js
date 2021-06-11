7ebe1a2d8b5d260dce80be5a4b4b597165f71f7b
Jonathan <me.info.jonathan@gmail.com>
2021-06-10 22:11:11 -0300

    exercicios-loops

---
+++ quinzena2/loops/index.js
@@ -0,0 +1,67 @@
+//Exercícios de interpretação de código
+
+//1.
+
+//O código está . O valor impresso é 10.
+
+//--------------------------------------------------------
+//2.
+//a.
+/*19
+  21
+  23
+  25
+  27
+  30*/
+
+//b.
+//muda a linha if (numero > 18) para if (numero[i] > 18)
+
+//--------------------------------------------------------
+//3.
+
+// Será impresso a quantidade de linhas com asterisco
+// *
+// **
+// ***
+// ****
+
+//---------------------------------------------------------
+//---------------------------------------------------------
+
+//Exercícios de escrita de código
+
+//1.
+// let bichinho = Number(prompt("Quantos bichinhos de estimação você tem?"))
+// let arrayPets = []
+// let nomePet = ""
+// if (bichinho === 0 ){
+//     console.log("Que pena! Você pode adotar um pet!")
+// }else{
+//         for(i = 0; i < bichinho; i++){
+//         nomePet = prompt(`Que legal, digite o nome do seu ${i+1}º pet`)
+//         arrayPets.push(nomePet)
+//     }
+//     console.log("Seus pets são:", arrayPets)
+// }
+
+//---------------------------------------------------------------------------
+
+//2.
+// let arrayOriginal = [1, 2, 3, 4, 5, 6, 7]
+// function imprimir (arrayNumeros) {
+//   for (let i = 0; i < arrayNumeros.length; i++) {
+//       console.log(arrayNumeros[i])
+//   }
+// }
+// imprimir(arrayOriginal)
+
+// for (let numero of arrayOriginal) {
+//     if (numero > maiorNumero)
+//     console.log(`Maior número ${maiorNumero}`)
+// }
+
+// if(numero < menorNumero) {
+//   menorNumero = numero
+//   console.log(`Menor número ${menorNumero}`)
+// }
