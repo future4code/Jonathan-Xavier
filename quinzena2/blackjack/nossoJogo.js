d994d848133bd004dba756090d23639aa1f86f72
Jonathan <me.info.jonathan@gmail.com>
2021-06-10 23:41:35 -0300

    projeto-quinzenal-andamento

--- quinzena2/blackjack/nossoJogo.js
+++ quinzena2/blackjack/nossoJogo.js
@@ -11,4 +11,26 @@
  * 
  */
 
-    console.log("Bem vindo ao jogo Blackjack")
+   //  console.log("Bem vindo ao jogo Blackjack")
+
+   //  if(confirm("Quer iniciar uma nova rodada?")) {
+   //    console.log("Iniciando partida")// o que fazer se o usuário clicar "ok"
+   // } else {
+   //    console.log("O jogo acabou!")// o que fazer se o usuário clicar "cancelar"
+   // }
+
+   // const carta = comprarCarta(); //Sorteia uma carta
+   //  console.log("Cartas Usuário:", carta.texto, "pontuação:", carta.valor) // imprime o naipe da carta e valor da carta do usuário
+
+   // const carta2 = comprarCarta(); // Cartas do computador
+    
+   // console.log("Cartas computador:", carta2.texto, "pontuação:", carta2.valor)
+
+   // if (carta.valor > carta2.valor) {
+   //    console.log("o Usuário ganhou!");
+   // }if (carta.valor < carta2.valor) {
+   //    console.log("o computador ganhou!");
+   // }if (carta.valor === carta2.valor) {
+   //    console.log("Empate!")
+   // }
+
