396ace38b616c074dea37aea756fb4d874fd8a40
Jonathan <me.info.jonathan@gmail.com>
2021-06-13 15:16:51 -0300

    modificacoes-projeto

---
+++ quinzena2/blackjack/desafio.js
@@ -0,0 +1,39 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+   //  console.log("Bem vindo ao jogo Blackjack")
+
+   //  if(confirm("Quer iniciar uma nova rodada?")) {
+   //    console.log("Iniciando partida")// o que fazer se o usuário clicar "ok"
+   // } else {
+   //    console.log("O jogo acabou!")// o que fazer se o usuário clicar "cancelar"
+   // }
+
+   // const carta = comprarCarta(); //Sorteia uma carta
+   // const carta2 = comprarCarta();
+   // const carta3 = comprarCarta();
+
+   //  console.log("Cartas Usuário:", carta.texto + carta2.texto + carta3.texto, "pontuação:", carta.valor + carta2.valor + carta3.valor) // imprime o naipe da carta e valor da carta do usuário
+  
+   //  // Cartas do computador
+   //  console.log("Cartas computador:", carta3.texto, "pontuação:", carta3.valor)
+   
+   // if (carta.valor > carta2.valor) {
+   //    console.log("o Usuário ganhou!");
+   // }if (carta.valor < carta2.valor) {
+   //    console.log("o computador ganhou!");
+   // }if (carta.valor === carta2.valor) {
+   //    console.log("Empate!")
+   // }
+
+   if (carta.texto)
