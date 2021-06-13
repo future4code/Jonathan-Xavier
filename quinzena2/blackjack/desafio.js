d39bbd3a1ecd809f6663f74680b077b2e96575b0
Jonathan <me.info.jonathan@gmail.com>
2021-06-13 16:02:45 -0300

    mudancas-projeto

--- quinzena2/blackjack/desafio.js
+++ quinzena2/blackjack/desafio.js
@@ -11,29 +11,38 @@
  * 
  */
 
-   //  console.log("Bem vindo ao jogo Blackjack")
+  console.log("Bem vindo ao jogo Blackjack")
+    
+    let jogo = confirm("Quer iniciar uma nova rodada?")
+    let carta1Usuario = comprarCarta()
+    let carta2Usuario = comprarCarta()
+    let carta1Pc = comprarCarta()
+    let carta2Pc = comprarCarta()
 
-   //  if(confirm("Quer iniciar uma nova rodada?")) {
-   //    console.log("Iniciando partida")// o que fazer se o usuário clicar "ok"
-   // } else {
-   //    console.log("O jogo acabou!")// o que fazer se o usuário clicar "cancelar"
-   // }
+    if (jogo) {
+       let carta1Usuario = comprarCarta()
+       let carta2Usuario = comprarCarta()
+       let carta1Pc = comprarCarta()
+       let carta2Pc = comprarCarta()
 
-   // const carta = comprarCarta(); //Sorteia uma carta
-   // const carta2 = comprarCarta();
-   // const carta3 = comprarCarta();
+      if (carta1Usuario.valor, carta2Usuario.valor, carta1Pc.valor, carta2Pc.valor === 22){
+            let sorteio = confirm("Sorteie novamente", comprarCarta())
+   } else {
+         console.log("Jogo Válido")
+   }
 
-   //  console.log("Cartas Usuário:", carta.texto + carta2.texto + carta3.texto, "pontuação:", carta.valor + carta2.valor + carta3.valor) // imprime o naipe da carta e valor da carta do usuário
-  
-   //  // Cartas do computador
-   //  console.log("Cartas computador:", carta3.texto, "pontuação:", carta3.valor)
-   
-   // if (carta.valor > carta2.valor) {
-   //    console.log("o Usuário ganhou!");
-   // }if (carta.valor < carta2.valor) {
-   //    console.log("o computador ganhou!");
-   // }if (carta.valor === carta2.valor) {
-   //    console.log("Empate!")
-   // }
+       let pontuacaoUsuario = carta1Usuario.valor + carta2Usuario.valor
+       let pontuacaoPc = carta1Pc.valor + carta2Pc.valor
 
-   if (carta.texto)
+       console.log(`Usuário - cartas ${carta1Usuario.texto} ${carta2Usuario.texto} - ${pontuacaoUsuario}`)
+       console.log(`Computador - cartas ${carta1Pc.texto} ${carta2Pc.texto} - ${pontuacaoPc}`)
+
+       if (pontuacaoUsuario > pontuacaoPc) {
+          console.log("O usuário ganhou!")
+       } else if (pontuacaoPc > pontuacaoUsuario) {
+          console.log("O computador ganhou!")
+       } else (pontuacaoUsuario === pontuacaoPc)
+          console.log("Empate!")
+       } else {
+      console.log("O jogo acabou!")
+}
