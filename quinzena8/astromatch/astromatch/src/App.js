75549d4d79333c3ce1727b0c8221e5a0e986346d
Jonathan <me.info.jonathan@gmail.com>
2021-09-02 07:31:11 -0300

    Criando botões de mudanca de tela

--- quinzena8/astromatch/astromatch/src/App.js
+++ quinzena8/astromatch/astromatch/src/App.js
@@ -1,14 +1,40 @@
 import React from "react"
 import { HomePage } from "./pages/HomePage/HomePage"
 import { MatchesPage } from "./pages/MatchesPage/MatchesPage"
+import styled from "styled-components";
 
-const App = () => {
-  return(
-    <div>Astromach
-      <HomePage />
-      <MatchesPage />
-    </div>
-  )
+const homePageDesign = styled.div`
+    background-color: violet;
+`;
+
+export default class App extends React.Component {
+  state = {
+    homePage: "HomePage"
+  };
+  
+  chooseScreen = () => {
+    switch (this.state.homePage) {
+      case "HomePage":
+        return <HomePage />;
+      case "MatchesPage":
+        return <MatchesPage />;
+      default: 
+        return <HomePage />;
+    }
+  };
+
+
+  changeScreen = (nameScreen) => {
+    this.setState({ chooseScreen: nameScreen });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={() => this.changeScreen("MatchesPage")}>Matches</button>
+        <button onClick={() => this.changeScreen("HomePage")}>HomePage</button>
+        {this.chooseScreen()}
+      </div>
+    );
+  }
 }
-
-export default App
--- quinzena8/astromatch/astromatch/src/pages/HomePage/HomePage.js
+++ quinzena8/astromatch/astromatch/src/pages/HomePage/HomePage.js
@@ -1,4 +1,10 @@
 import React from "react"
+import styled from "styled-components"
+
+const Background = styled.div`
+    background-color: green;
+    align-items: center;
+`;
 
 export const HomePage = () => {
     return(
