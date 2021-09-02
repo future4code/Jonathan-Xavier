47083a8f217509077846b27fe90abf338705cdf1
Jonathan <me.info.jonathan@gmail.com>
2021-09-01 21:50:35 -0300

    iniciando astromatch

---
+++ quinzena8/astromatch/astromatch/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { HomePage } from "./pages/HomePage/HomePage"
+import { MatchesPage } from "./pages/MatchesPage/MatchesPage"
+
+const App = () => {
+  return(
+    <div>Astromach
+      <HomePage />
+      <MatchesPage />
+    </div>
+  )
+}
+
+export default App
